b050fbfe00ce6946e5701a06b1f44cea20da726a
David Blackmon <davidkblackmon@gmail.com>
2018-03-26 07:48:34 -0400

    reductions

--- lib/simple/expression.rb
+++ lib/simple/expression.rb
@@ -13,7 +13,13 @@ class Expression
   end
 
   def reduce
-    raise "Not Implemented"
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
   end
 
   def to_s
--- lib/simple/multiply.rb
+++ lib/simple/multiply.rb
@@ -4,4 +4,14 @@ class Multiply < Expression
   def to_s
     "#{left} * #{right}"
   end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
 end
